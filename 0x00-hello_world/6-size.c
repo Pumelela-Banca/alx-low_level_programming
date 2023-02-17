d05f3d26aa5cbbf93ebc4cb2941c6a93c942b3da
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-16 16:47:46 -0800

    add Q8

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 	long long int b;
 
 	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(a));
+	printf("Size of a long long: %lu byte(s)\n",(unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 	return (0);
 }
