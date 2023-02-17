152756cdda0c74c98bae6f8746fde14038c7f4f4
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-16 16:02:32 -0800

    3  more questions

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
-/*Hello, printf*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	/*
-	 * This Function prints
-	 */
-
 	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the sizes of all the types.
+ *
+ * Return: 0 To show complete.
+ */
+
+int main(void)
+{
+	float f;
+	char c;
+	int i;
+	long int a;
+	long long int b;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	return (0);
+}
