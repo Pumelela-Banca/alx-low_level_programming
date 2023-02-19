81996b6760b35baec770fc39b1536e62f5d9be1c
Banca-Pumelela <pumelelabanca@gmail.com>
2023-02-18 17:05:09 -0800

    Q0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a, b;
+
+	b = 379;
+
+	for (a = 353; a < b; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
