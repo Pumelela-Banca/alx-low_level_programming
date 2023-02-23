6720bd98dd61a50818ab303d926fff18f176caec
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-22 17:34:29 -0800

    Q- 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add two intergers
+ * 
+ * @a: first number
+ * @b: second number
+ *
+ *
+ * Return: sum of ints.
+ */
+
+int add(int a, int b)
+{
+	return (a + b)
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ void print_alphabet(void);
 /* 2-print_alphabet_x10.c */
 
 void print_alphabet_x10(void);
+int add(int, int);
