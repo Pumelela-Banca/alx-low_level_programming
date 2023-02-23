37b1acc441b1ea3a0a94f9f97a2c502efa47d767
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-22 18:12:43 -0800

    Q-11 1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,31 +3,31 @@
 /**
  * print_to_98 - Print  numbers to 98
  *
+ * @n: integer for range
+ *
  * Return: void
  */
 
 void print_to_98(int n)
 {
-	int scope;
-
 	if (n < 98)
 	{
-		for(n; n == 97; n++)
+		for (n; n == 97; n++)
 		{
-			printf("%d, ",n)
+			printf("%d, ", n);
 		}
-		printf("98\n")
+		printf("98\n");
 	}
 	else if (n > 98)
 	{
-		for(n; n == 99; n--)
-                {
-                        printf("%d, ",n)
-                }
-                printf("98\n")
+		for (n; n == 99; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("98\n");
 	}
 	else
 	{
-		printf("98\n")
+		printf("98\n");
 	}
 }
