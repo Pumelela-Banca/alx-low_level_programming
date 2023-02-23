da2e789b04d60b2e69eefad7230cf0ff227c5476
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-22 17:16:43 -0800

    main file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,3 @@
-#include "_putchar.c"
-#include "0-putchar.c"
-#include "1-alphabet.c"
-#include "2-print_alphabet_x10.c"
-#include "3-islower.c"
-#include "4-isalpha.c"
-#include "5-sign.c"
-#include "6-abs.c
-#include "7-print_last_digit.c"
-#include "8-24_hours.c"
-#include "9-times_table.c"
-#include "11-print_to_98.c"
-#include "10-add.c"
-
 /* Function prototypes */
 
 /*_putchar*/
