3c5ce002c9e8a75d8270e4a6115d640cde6fd584
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-24 15:20:12 -0800

    All questions-4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,9 +11,9 @@
 #include "10-print_triangle.c"
 
 
-int _isupper(char c);
-int _putchar(char c);
+int _isupper(int c);
 int _isdigit(int c);
+int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
