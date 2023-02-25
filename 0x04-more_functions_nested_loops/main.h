a37b7a534f88d7c3a9aafc3afbc84f8efd13a21c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-24 16:58:23 -0800

    all again

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,14 +9,14 @@
 #include "7-print_diagonal.c"
 #include "8-print_square.c"
 #include "10-print_triangle.c"
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int);
-void print_diagonal(int);
-void print_square(int);
-void print_triangle(int);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
