ae14c58f01eaa8262c600b64a8c0a3a271e83220
Pumelela-Banca <pumelelabanca@gmail.com>
2023-02-24 18:44:53 -0800

    all 7

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include "_putchar.c"
+#include "<unistd.h>"
 
 int _putchar(char c);
-#endif
 
-#include "0-isupper.c"
-#include "1-isdigit.c"
-#include "2-mul.c"
-#include "3-print_numbers.c"
-#include "4-print_most_numbers.c"
-#include "5-more_numbers.c"
-#include "6-print_line.c"
-#include "7-print_diagonal.c"
-#include "8-print_square.c"
-#include "10-print_triangle.c"
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -25,3 +14,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif
