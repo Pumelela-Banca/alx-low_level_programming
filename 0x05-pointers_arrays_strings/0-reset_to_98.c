e408e50a335002188b473fe8653b87fb3c98ea87
Pumelela Banca <pumelelabanca@gmail.com>
2023-02-28 00:21:45 +0200

    Q-0 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,10 @@
 /**
  * reset_to_98 - change value of n to 98
  *
- * @*n: pointer address to be changed.
+ * @n: pointer address to be changed.
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
