68dc2648c253105abb691e9d38c3c5c89f3eaedc
Pumelela Banca <pumelelabanca@gmail.com>
2023-02-28 00:33:51 +0200

    Add swap.c and prototype to main..h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void swap_int - swap two ints
+ *
+ * @a: integer one
+ * @b: integer two
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *hold;
+
+	*hold = *a;
+	*a = *b;
+	*b = *hold;
+}
