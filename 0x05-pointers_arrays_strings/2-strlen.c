30f74599895d519a320f96b6db9c6bd90a348809
Pumelela Banca <pumelelabanca@gmail.com>
2023-02-28 01:06:59 +0200

    add 2-strlen.c its prototype on main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - count len of string
+ *
+ * @s: string to be counted
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len, stop;
+
+	for (len = 1; len < 1000; len++)
+	{
+		stop = *(s + len);
+		if (stop == '\0')
+		{
+			return (len);
+		}
+	}
+}
