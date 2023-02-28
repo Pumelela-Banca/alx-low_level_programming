f96a6a761a0260655db45c27d8dce8ad0175f4ed
Pumelela Banca <pumelelabanca@gmail.com>
2023-02-28 18:05:57 +0200

    5-revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,51 +10,37 @@ void rev_string(char *s)
 {
 	int len, stop, size, hold;
 
+	char keep[1000];
+
 	for (len = 0; len < 1000; len++)
 	{
 		if (*s == '\0')
 		{
-			*s = "";
+			s = "";
 			break;
 		}
 
 		stop = *(s + len);
+		keep[len] = stop;
 
 		if (stop == '\0')
 		{
 			size = len - 1;
+			hold = 0;
 			while (size >  -1)
 			{
 				if (size == 0)
 				{
-					hold = *s;
+					 s[hold] = *keep;
 				}
 				else
 				{
-					hold = *(s + size);
+					s[hold] = *(keep + size);
 				}
-				_putchar(hold);
+				hold++;
 				size--;
 			}
-			_putchar('\n');
 			break;
 		}
 	}
-}
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
 }
