288a0b427fa05866c252f125a024e7453cf86456
Pumelela Banca <pumelelabanca@gmail.com>
2023-02-28 18:48:21 +0200

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,40 @@ void puts_half(char *str);
 {
 	int n, loc;
 
+	char position;
+
 	for (n = 0; n < 1000; n++)
-	{
-		loc =  
-		if (
+	{  
+		if (*str == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+
+		position = str[n];
+
+		if (position == '\0')
+		{
+			if (n == 2)
+			{
+				_putchar(str[n]);
+			}
+			if (n % 2 == 0)
+			{
+				loc = 2;
+			}
+			else
+			{
+				loc = (n - 1) / 2;
+			}
+			while (loc < n)
+			{
+				_putchar(str[n]);
+				loc++;
+			}
+			_putchar('\n');
+			break;
+		}
+	}
+}
+
