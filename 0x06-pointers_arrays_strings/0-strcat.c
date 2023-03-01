011ce7236398d903b492f9f7a4fbbb70f4855df6
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-02 01:07:59 +0200

    add Readme, created folder and Q-0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat- Concatenate, dest with src
+ *
+ * @dest: add src
+ * @src: add to dest
+ *
+ * Description: add src to dest and return string of both
+ *
+ * Return: char * (string concatenation).
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int size, addon;
+
+	char hold;
+
+	for (size = 0; size < 1000; size++)
+	{
+		hold = dest[size];
+		if ( hold == '\0')
+		{
+			break;
+		}
+	}
+	for (addon = 0; addon < size && src[addon] != '\0'; addon++)
+	{
+		dest[size + addon] = src[addon];
+	}
+	dest[size + addon] = '\0';
+	return dest;
+}
