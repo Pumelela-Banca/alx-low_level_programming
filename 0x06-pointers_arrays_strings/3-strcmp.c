1083f7022e7c4c4674ebc7870341e8e6132cf207
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-02 02:52:43 +0200

    add Q-4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp- compare two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: int 0 for equal, -1 for s1 < s2, 1 for s1 > s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array and using number of elements
+ *
+ * @a: array to be reversed
+ * @n: number of elements in array
+ *
+ * Return: void.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int limit, bottom;
+
+	char hold[1000];
+
+	for (limit = 0; limit < n && a[limit] != '\0'; limit++)
+	{
+		hold[limit] = a[limit];
+	}
+	bottom = 0;
+	while(limit > -1)
+	{
+		a[limit] = hold[bottom];
+		limit--;
+		bottom++;
+	}
+}
