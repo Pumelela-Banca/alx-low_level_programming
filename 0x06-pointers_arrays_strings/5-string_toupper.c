31e127d72609b7a602c71c47aeb2168a9e8a14d2
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-03 01:46:30 +0200

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * string_topper - Turn all characters into upper case
+ *
+ * @char *: letters  to be turned into upper case
+ *
+ * Return: char *string of upper case letters
+ */
+
+char *string_topper(char *)
+{
+    *
