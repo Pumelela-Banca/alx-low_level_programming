39cbdb6f3383b85e45c19017f29f842d9287a161
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-02 02:01:35 +0200

    Q-2 0

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy- copy string one to another
+ *
+ * @dest: copy src to dest
+ * @src: copy to dest
+ * @n: number of bytes to copy
+ *
+ * Return: char * (dest)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int addon;
+
+	char hold;
+	
+	for (addon = 0; addon < n && src[addon] != '\0'; addon++)
+	{
+		dest[addon] = src[addon];
+	}
+	for ( ; addon < n; addon++)
+	{
+		dest[addon] = '\0';
+	}
+	return (dest);
+}
