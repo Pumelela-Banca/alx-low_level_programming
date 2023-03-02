855ad2a563d63f216a2fa7510eb583e208011c54
Pumelela-Banca <pumelelabanca@gmail.com>
2023-03-02 10:32:02 -0800

    q-3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,62 +1,21 @@
 #include "main.h"
 
 /**
- * _strcmp- compare two strings
+ * _strcmp - copare two strings
  *
- * Description: Strings to be compared using _strcmp.
- * @s1: first string
- * @s2: second string
+ * @s1: first input
+ * @s2: second input
  *
- * Return: int 0 for equal, -1 for s1 < s2, 1 for s1 > s2.
+ * Return: int value and the differences
  */
-
-int look_up(char c);
 
 int _strcmp(char *s1, char *s2)
 {
-	int count1, count2, vals1, vals2;
-	char hold;
+	int i = 0;
 
-	vals1 = 0;
-	vals2 = 0;
-
-	for (count1 = 0;  (s1[count1]) != '\0'; count1++)
+	while ((s1[i] - s2[i] == 0) && (s1[i] != '\0'))
 	{
-		hold = s1[count1];
-		vals1 = vals1 + look_up(hold);
-		count1++;
+		i++;
 	}
-	for (count2 = 0;  (s2[count2]) != '\0'; count2++)
-	{
-		hold = s1[count2];
-		vals2 = vals2 + look_up(hold);
-		count2++;
-	}
-	return (vals1 - vals2);
-}
-
-/**
- * look_up- look up ascii VALUE for char
- *
- * Description: character to ascii look up.
- * @c : char to be looked at.
- *
- * Return: int, ascii value of c
-*/
-
-int look_up(char c)
-{
-	int ho;
-	char names[91] = "!\"#$%&'()*+,-."
-		"/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz";
-
-	for (ho = 0; ho < 91; ho++)
-	{
-		if (c == names[ho])
-		{
-			break;
-		}
-		ho++;
-	}
-	return (ho + 33);
+	return (s1[i] - s2[i]);
 }
