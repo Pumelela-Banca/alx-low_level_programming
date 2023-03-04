a3cd1c1f8e4e53eefdebde5256d774cc0947f8df
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-04 02:30:28 +0200

    add 7-leet.c 0

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,6 +8,22 @@
  * Return: Char string.
  */
 
-char *leet(char *)
+char *leet(char *na)
 {
+	int i, c;
+	int nums[] = {4, 3, 0, 7, 1};
+	char smll[] = {'a', 'e', 'o', 't', 'l'};
+	char cpl[] = {'A', 'E', 'O', 'T', 'L'};
 
+	for (i = 0; na[i] != '\0'; i++)
+	{
+		for (c = 0; c < 5; c++)
+		{
+			if (na[i] == cpl[c] || na[i] == smll[c])
+			{
+				na[i] = nums[c];
+			}
+		}
+	}
+	return (na);
+}
