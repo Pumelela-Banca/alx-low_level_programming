c42e1eacb757851551a85ff5bfbb133b5f33441c
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-07 23:49:38 +0200

    _strt 4

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, c;
+	int i, c, count;
 
 	i = 0;
+	count = 0;
+
+	while (needle[count] != '\0')
+	{
+		count++;
+	}
+	if (count <= 1)
+		return (haystack + count);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (c = 0; needle[c] != '\0'; c++)
 		{
-			if (needle[c] == haystack[i] 
-					&& needle[c + 1] == haystack[i + 1]
-					&& needle[c + 2] == haystack[i + 2])
+			if (needle[c] == haystack[i]
+					&& needle[c + count] == haystack[i + count])
 			{
 				return (haystack + i);
 			}
