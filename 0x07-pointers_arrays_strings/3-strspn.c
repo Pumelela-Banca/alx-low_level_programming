2e51c100a34b5fe0d18ff25e166dc712341942c1
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-07 11:33:06 +0200

    add 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn- 
+ * _strspn- get length of  a prefix substring
+ *
+ * @s: search for bytes from here
+ * @accept: search for  these  bytes in s
+ *
+ * Return: int with the number of bytes.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count, ind, num, in;
+
+	num = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		in = 0;
+		for (ind = 0; accept[ind] != '\0'; ind++)
+		{
+			if (s[count] == accept[ind])
+			{
+				num++;
+				in = 1;
+			}
+		}
+		if (in == 0)
+		{
+			break;
+		}
+	}
+	return (num);
+}
