dfdfd586ef63756c458f9de6b573f4d89049f33e
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-09 12:06:06 +0200

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,5 +8,47 @@
  * Return: the natural square root or -1 if not present.
  */
 
+int sqrt_count(int num, int count);
+
 int _sqrt_recursion(int n)
 {
+	int i;
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+
+	i = sqrt_count(n, 1);
+
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * sqr_count - count if number is natural square root
+ *
+ * @num: subtract from number until 0.
+ * @odd: number to subtract
+ *
+ * Return: int 1 for positive,  -1 for not natural sqrt.
+ */
+
+int sqrt_count(int num, int count)
+{
+	if (num <= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + sqrt_count(num - count, count + 2));
+	}
+}
+
