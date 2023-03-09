077bac0b997b06abd16542b3e41bbbadb1225b8a
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-10 01:03:14 +0200

    100-is_palindrome.c  0

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * compc - compare two characters
+ *
+ * @s - string
+ * @down - go down from word
+ * @up -  go up in word
+ *
+ * Return: 1 for true  0 for false
+ */
+
+int compc(char *s, int down, int up)
+{
+	if (s[down] == s[up])
+	{
+		return (1);
+	}
+	else if (s[down] != s[up])
+	{
+		return (0);
+	}
+	else
+	{
+		return (compc(*s, down - 1, up + 1));
+	}
+}
+
+/**
+ *
+ * is_palindrome - check if string is a palindrome
+ *
+ * @s - string
+ *
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int strln;
+
+	strln = _strlen_recursion(s);
+
+	if (s[0] == '\0')
+		return (1);
+
+	compc(s, strln, 0);
+}
