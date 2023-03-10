385651795147c95fa8eedf9561b9a5e9911da3bd
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-10 18:24:57 +0200

    3-mul.c 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,14 +5,14 @@
  * main - entry point to multiplication function
  *
  * @argc: len of arguments array
- * @@argv: array  with arguments
+ * @argv: array  with arguments
  *
  * Return: 1 always
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc > 3)
+	if (argc > 3 || argc == 1)
 	{
 		printf("Error\n");
 		return (1);
