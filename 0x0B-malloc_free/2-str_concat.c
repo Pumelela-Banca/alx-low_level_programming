aa535131f5b8009f69e6dfaffa291619c53cf02a
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-14 21:13:59 +0200

    add three files 0, 1, 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,4 +9,44 @@
  */
 
 char *str_concat(char *s1, char *s2)
-{}
+{
+	int size1, size2, hold, start, next;
+	char *new;
+
+	next = 0;
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+		continue;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		continue;
+
+	if (size1 == 0 && size2 == 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		hold = size1 + size2;
+	}
+	new = malloc(sizeof(*new) * hold);
+	for (start = 0; start < hold; start++)
+	{
+		if (size1 == 0)
+		{
+			new[start] = s2[start];
+		}
+		else if (size2 == 0)
+		{
+			new[start] = s1[start];
+		}
+		if (start < size1)
+		{
+			new[start] = s1[start];
+		}
+		else if (s2[next] != '\0')
+		{
+			new[start] = s2[next];
+			next++;
+		}
+	}
+	return (new);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,4 +10,8 @@
 
 int **alloc_grid(int width, int height)
 {
+	if (width == 0 || height == 0)
+		return ('\0');
+
+	
 
