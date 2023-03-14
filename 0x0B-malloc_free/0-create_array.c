1f9b69091e64057c4fdae1d1a4151c18e6b52556
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-14 22:23:39 +0200

    add three files 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,11 @@ char *create_array(unsigned int size, char c)
 		return ('\0');
 	}
 
-	names = malloc(sizeof(*names) * size);
+	names = malloc(sizeof(char) * size);
 
 	for (start = 0; start < size; start++)
 	{
 		names[start] = c;
 	}
-	names[start] = '\0';
 	return (names);
 }
