fc29150116636169bcbd4bd75a3d29f8e860424a
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-18 01:29:14 +0200

    fixed main.h file, with propper syntax

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,6 @@
-/* Function prototypes */
+#ifndef MAIN_H
+#define MAIN_H
 
-/*_putchar*/
+int _putchar(char c);
 
-int _putchar(char);
-
-/* 1-alphabet.c */
-
-void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
-
-void print_alphabet_x10(void);
-int add(int, int);
-void print_to_98(int n);
-int print_last_digit(int);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-void jack_bauer(void);
+#endif
