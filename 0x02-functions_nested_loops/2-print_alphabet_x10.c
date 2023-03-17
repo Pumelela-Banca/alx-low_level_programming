96204f0ec65862f84355969410aee805cc810fd9
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-18 01:48:59 +0200

    added prototype to main.h for 10xAlphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
+#include "main.h"
+
 /**
  * print_alphabet_x10 - Print alphabet 10X
  *
  * Return: void, no return
  */
-
-void print_alphabet_x10(void);
 
 void print_alphabet_x10(void)
 {
@@ -15,4 +15,20 @@ void print_alphabet_x10(void)
 		print_alphabet();
 		a--;
 	}
+}
+
+/**
+* print_alphabet - Print string using loop.
+*
+* Return: void, no retturn value.
+*/
+
+void print_alphabet(void)
+{
+	int a;
+	for (a =  97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
