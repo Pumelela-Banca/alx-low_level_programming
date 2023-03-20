c688afc93e70f2ba8c689ac3cfc50ae9322d8d5f
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-21 01:49:27 +0200

    added prototype to dog.h and file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog- Initialize dog
+ *
+ * @d: struct details
+ * @name: animal name
+ * @age: age of dog
+ * @owner: animal owner
+ *
+ * Return: Void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d-> name = name;
+	d-> age = age;
+	d-> owner = owner;
+}
