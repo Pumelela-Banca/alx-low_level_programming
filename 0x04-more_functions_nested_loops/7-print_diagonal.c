cc344f880af8d9d74fce976b4da3874b4a972a1a
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-20 02:07:01 +0200

    added conditional for 0 and below numbers

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,21 +14,18 @@ void print_diagonal(int n)
 
 	end = n + 1;
 
+	if (n <= 0)
+		_putchar('\n');
+
 	for (start = 1; start < end; start++)
 	{
-		if (n < 1)
-		{
-			_putchar('\n');
-			break;
-		}
-
 		end2 = start + 1;
 
 		for (start2 = 1; start2 < end2; start2++)
 		{
 			_putchar(32);
 		}
-		_putchar('\\');
+		_putchar(92);
 		_putchar('\n');
 	}
 }
