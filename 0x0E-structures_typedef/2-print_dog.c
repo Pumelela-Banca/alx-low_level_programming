982e5d8fa4e2324cae6089ec4b50d8fe538a749b
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-22 00:07:50 +0200

    add Q-5 only freed struct not every. Q-1 more spelling fixes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,17 +17,17 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL || d->name[0] == '\0')
 	{
-		printf("Name: nil\nAge: %f\nOwner: %s", 
+		printf("Name: (nil)\nAge: %f\nOwner: %s", 
 				d->age, d->owner);
 	}
 	else if (d->age < 1)
 	{
-		printf("Name: %s\nAge: nil\nOwner: %s",
+		printf("Name: %s\nAge: (nil)\nOwner: %s",
                                 d->name, d->owner);
 	}
 	else if (d->owner == NULL || d->owner[0] == '\0')
 	{
-		printf("Name: %s\nAge: %f\nOwner: nil",
+		printf("Name: %s\nAge: %f\nOwner: (nil)",
                                 d->name, d->age);
 	}
 	else
