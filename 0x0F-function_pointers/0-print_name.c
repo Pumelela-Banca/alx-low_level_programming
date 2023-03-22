d72f880eb5dec7016d08fb726b491b14bf179cb4
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-22 23:16:55 +0200

    add function_pointers.h, README.md 0-print_name.c 1-array_iterator.c files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a function name
+ *
+ * @name: chars to print
+ * @f: function pointer
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator- execute function on every given input of array
+ *
+ * @array: data to use
+ * @size: size of array
+ * @action: instructions to use
+ *
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int start;
+
+	for (start = 0; start < size; start++)
+	{
+		(*action)(array[start]);
+	}
+}
