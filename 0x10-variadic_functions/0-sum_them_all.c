92c0a99e924cbda3b7a5bdc923d37f4a48e1dff9
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-23 19:03:28 +0200

    add initialization, test and .h files with Q-0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all its parameters
+ *
+ * @n: number of parameters
+ * @...: numbers to sum up
+ *
+ * Return: sum of all numbers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	if (n < 1)
+		return (0);
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return sum;
+}
