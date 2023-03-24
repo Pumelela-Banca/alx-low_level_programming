2cd1b9fe95002f6669a60085498e84b19eab09c4
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-24 03:17:41 +0200

    add  file Q-3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,4 +11,43 @@
 
 void print_all(const char * const format, ...)
 {
+	int i, j; float f; char *str; char c;
+	const char *names;
+	char *sep;
+
+	va_list pa;
+
+	va_start(pa, format);
+	sep = "";
+	names = format;
+	j = 0;
+	while (names[j] != '\0')
+	{
+		switch (names[j])
+		{
+			case 'f':
+				f = va_arg(pa, double);
+				printf("%s%f", sep , f);
+				break;
+			case 'd':
+				i = va_arg(pa, int);
+				printf("%s%d", sep , i);
+				break;
+			case 'c':
+				c = va_arg(pa, int);
+				printf("%s%c", sep ,c);
+				break;
+			case 's':
+				str = va_arg(pa, char *);
+				if (str == NULL)
+				{
+					str = "nil";
+				}
+				printf("%s%s", sep, str);
+		}
+		sep = ", ";
+		j++;
+	}
+	va_end(pa);
+	printf("\n");
 }
