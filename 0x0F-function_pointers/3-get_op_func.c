7a89c321d1a3735e230a8283915d822d3fbb44c6
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-25 02:07:39 +0200

    submit Q-3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ *
+ * get_op_func - Select correct function to perfom action
+ *
+ * @s: operand to use
+ *
+ * Return: IInt value.
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+	i = 0;
+	while ( i < 6)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main- clalculator interface
+ *
+ * @argc: len of arguments
+ * @argv: array of all arguments
+ *
+ * Return: int for  exit status
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strcmp(argv[2], "+") != 0 || strcmp(argv[2], "-") != 0 || 
+			(strcmp(argv[2], "*") != 0) || (strcmp(argv[2], "/") != 0)
+			|| (strcmp(argv[2], "%") != 0))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (((atoi(argv[3]) == 0) && (strcmp(argv[2], "/") == 0)) || 
+			((atoi(argv[3]) == 0) && (strcmp(argv[2], "%") == 0)))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	return (1);
+}
