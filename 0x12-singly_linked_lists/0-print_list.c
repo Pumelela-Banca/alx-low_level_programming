0b75b2a32b9ba32fa98fef1a6d00e04256d6419f
Pumelela Banca <pumelelabanca@gmail.com>
2023-03-30 17:57:39 +0200

    Initial commit readme.md, test files and Q-0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements of list_t
+ *
+ * @h: struct data, with char string, len of string and pointer to next item
+ *
+ * Return: size_t, number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t cc = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		h = h->next;
+		return (cc);
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		cc++;
+	}
+	return (cc);
+}
