32b469a18aad2ab2ee1deb199fe6713221921a1d
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-04 13:13:57 +0200

    added base case for when list is empty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,16 +2,18 @@
 
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * 
+ *
  * @head: start of linked list
  * @index: Index of node to  return
- * 
+ *
  * Return: Node of list
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
+	if (head == NULL)
+		return (NULL);
 
 	if (index == 0)
 		return (head);
