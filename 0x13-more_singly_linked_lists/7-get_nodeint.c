03c2abdd63c742288602e967c99fdc16763c110c
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-04 13:05:57 +0200

    Q-7, function returns node on given index of a linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * 
+ * @head: start of linked list
+ * @index: Index of node to  return
+ * 
+ * Return: Node of list
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (index == 0)
+		return (head);
+
+	i = 1;
+	while (head->next != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
