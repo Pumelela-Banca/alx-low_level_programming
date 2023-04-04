21443f57234304f0a16620b6a83f06bf7fce8ef4
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-04 02:31:02 +0200

    Q-6 used free function to delete

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * pop_listint - delete head node of a list
@@ -13,13 +14,18 @@ int pop_listint(listint_t **head)
 	listint_t *new;
 
 	if (*head == NULL)
+	{
+		free(*head);
 		return (0);
+	}
 	new = *head;
 	if (new->next == NULL)
 	{
 		*head = NULL;
+		free(*head);
 		return (new->n);
 	}
 	*head = new->next;
+	free(new);
 	return (new->n);
 }
