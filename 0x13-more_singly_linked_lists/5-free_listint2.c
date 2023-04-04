9f1be10d2fed302cb39d726535b56b250e6faef4
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-04 02:01:59 +0200

    added Q-5 function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free memory
+ *
+ * @head: head of list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}
+
+/**
+ *
+ * free_listint - free list of ints
+ *
+ * @head: first item on list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+        listint_t *hold;
+
+        if (head == NULL)
+                return;
+
+        while (head != NULL)
+        {
+                hold = head;
+                head = head->next;
+                free(hold);
+        }
+}
