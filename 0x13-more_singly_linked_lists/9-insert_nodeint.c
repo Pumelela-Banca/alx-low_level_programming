d789121fa27e8584a73e6bfb0ae8c92d5fe758f0
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-05 18:11:01 +0200

    Q-9 added removed add_nodeto_end fucntion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -97,38 +97,6 @@ size_t listint_len(const listint_t *h)
 }
 
 /**
- * add_nodeint_end - adds node to end of function
- *
- * @head: list to add to
- * @n: data in node
- *
- * Return: address of new element
- */
-
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new, *last;
-
-	last = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
-	while (last->next != NULL)
-	{
-		last = last->next;
-	}
-	last->next = new;
-	return (new);
-}
-
-/**
  * add_nodeint - add node to start of list
  *
  * @head: head of list
