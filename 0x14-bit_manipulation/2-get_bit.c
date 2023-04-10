5a7f91585777b65c969f6dfb507033ffe90139fb
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-10 15:37:52 +0200

    function returns byte at required index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * get_bit - returns the value of a bit at a given index
@@ -11,4 +12,18 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int *bin;
+	int i;
+
+	bin = malloc(sizeof(int) * 32);
+	if (bin == NULL)
+		return (-1);
+	i = 0;
+	while (n > 0)
+	{
+		bin[i] = n % 2;
+		n /= 2;
+		i++;
+	}
+	return (bin[index]);
 }
