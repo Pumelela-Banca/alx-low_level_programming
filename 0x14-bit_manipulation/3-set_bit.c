e54779b096fc65ebc21d5471f704c2959e12cd67
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-10 16:11:08 +0200

    added error handling  for NULL poiner

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index
@@ -12,8 +12,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
-		return (-1); 
+	if ((n == NULL) || (index > 31))
+		return (-1);
 	*n |= (1 << index);
 	return (1);
 }
