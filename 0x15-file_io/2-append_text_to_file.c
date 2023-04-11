e6e0d7cf20ec17725c01c7ccaeec3a1b1929bfdb
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 01:26:03 +0200

    Q-2 file appends to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
 
 /**
  * append_text_to_file - append text to end of file
@@ -11,4 +13,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+
+	int file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	ssize_t bytwrttn = write(file, text_content, sizeof(text_content) - 1);
+	if (bytwrttn < sizeof(text_content) - 1)
+		return (-1);
+	close(file);
+	return (1);
 }
