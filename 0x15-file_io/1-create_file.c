67feb261af27f810ca15a1d2be2a7e5929b008d8
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 00:29:20 +0200

    Q-1 removed write to for error in number of bytes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
-#include <unistd.h>
 
 /**
  * create_file - creates a file
@@ -27,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		i = fputs("", fptr);
 	else
 	{
-		i = write(fptr, text_content , sizeof(text_content) - 1);
+		i = write(text_content, fptr);
 	}
 	if (i < 0)
 		return (-1);
