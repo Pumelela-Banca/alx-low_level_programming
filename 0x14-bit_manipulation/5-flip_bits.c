0c5fa7b1d01402c88d790d53a1dbe339efad1af3
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-11 20:51:33 +0200

    Q-5 does the difference between two int bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * flip_bits - number of bits you would need to flip to get from one number to another 
@@ -11,4 +12,21 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	int m1[33], n1[33];
+	int cc, count, ccc;
+
+	for (cc = 31; cc > -1; cc--)
+	{
+		n1[cc] = ((n >> cc) & 1);
+		m1[cc] = ((m >> cc) & 1);
+	}
+	count = 0;
+	ccc = 0;
+	while (ccc < 32)
+	{
+		if (m1[ccc] != n1[ccc])
+			count++;
+		ccc++;
+	}
+	return (count);
 }
