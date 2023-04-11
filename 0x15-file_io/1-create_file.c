068d753c00172da52057a00f359b169468c45b89
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 00:01:16 +0200

    Q-1 added permisions for user

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 /**
  * create_file - creates a file
@@ -30,5 +31,6 @@ int create_file(const char *filename, char *text_content)
 	if (i < 0)
 		return (-1);
 	fclose(fptr);
+	chmod(filename, S_IRUSR | S_IWUSR);
 	return (1);
 }
