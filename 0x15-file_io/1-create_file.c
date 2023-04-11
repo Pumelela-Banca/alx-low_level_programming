a716c5534c055cf9cf086b43138001ff6fd9fd70
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 00:39:12 +0200

    Q-1 removed write to for error in number of bytes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		i = fputs("", fptr);
 	else
 	{
-		i = write(text_content, fptr);
+		i = fputs(text_content, fptr);
 	}
 	if (i < 0)
 		return (-1);
