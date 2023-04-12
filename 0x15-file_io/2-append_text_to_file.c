b3a3b63949e7bc435251f9d205745b4c3dcf3bdc
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 03:15:15 +0200

    Q-2 changed algorithim

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,21 +13,16 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int bytwrttn;
-	int file, len;
+	FILE *fptr;
+	int file;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = fopen(filename, "a");
 	if (file == -1)
-		return (-1);
-	for (len = 1; text_content[len] != '\0'; len++)
-	{}
-	bytwrttn = write(file, text_content, len - 1);
-	if (bytwrttn < len - 1)
 		return (-1);
 	close(file);
 	return (1);
