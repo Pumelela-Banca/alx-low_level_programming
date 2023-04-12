bbe36f755e1bea37b9e8f3374c4626b9725986e3
Pumelela Banca <pumelelabanca@gmail.com>
2023-04-12 03:21:29 +0200

    Q-2 changed algorithim

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * append_text_to_file - append text to end of file
@@ -14,16 +14,15 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *fptr;
-	int file;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
 
-	file = fopen(filename, "a");
-	if (file == -1)
+	fptr = fopen(filename, "a");
+	if (fptr == NULL)
 		return (-1);
-	close(file);
+	fclose(fptr);
 	return (1);
 }
