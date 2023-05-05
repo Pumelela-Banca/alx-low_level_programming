a4b917ef244ce4b727fe6c9a7744f78ac8b3ebe1
Banca-Pumelela <pumelelabannca@gmail.com>
2023-05-05 03:42:34 +0200

    changed insert at center to start coount at one

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next = cpy;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
@@ -68,7 +68,7 @@ listint_t *add_node_center(listint_t **head, const int n, unsigned int idx)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	c = 0;
+	c = 1;
 	while (c < (idx + 1))
 	{
 		if (c == idx)
