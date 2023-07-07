0925bfdc8756734e15d0e7690470ad165cc53a67
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-07 12:49:28 +0200

    created free function  to free array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,15 +14,38 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t *bb;
 	unsigned long int i;
 
+	table->array = array;
 	for (i = 0; i < size; i++)
 	{
 		bb = malloc(sizeof(hash_node_t));
+		if (bb == NULL)
+		{
+			free_array(array);
+			return (NULL);
+		}
 		bb->key = NULL;
 		bb->value = NULL;
 		bb->next = NULL;
 		array[i] = bb;
 	}
 	table->size = size;
-	table->array = array;
 	return (table);
+}
+
+/**
+ * free_array - frees hash table
+ *
+ * @array: array to free 
+ *
+ * Return: void
+ */
+
+void free_array(hash_node_t *array)
+{
+	int i = 0;
+
+	for (i; array[i] != '\0'; i++)
+	{
+		free(array[i]);
+	}
 }
