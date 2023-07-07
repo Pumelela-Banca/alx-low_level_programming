af0bb5d207b64a87123439d7acd60e8ecbcf7b77
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-07 02:21:26 +0200

    cretes  index from function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,23 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - gives index of a key
+ *
+ * @key: string to find
+ * @size: size of array
+ *
+ * Return: index to store (key/value) pair
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int num;
+
+	num = hash_djb2(key);
+	if (num > size - 1)
+	{
+		return (num % size);
+	}
+	else
+		return (num);
+}
