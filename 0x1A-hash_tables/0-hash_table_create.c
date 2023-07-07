8c5ee33c40c17551ff927335b12cccf60b3fb088
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-07 02:20:20 +0200

    creates hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,28 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of array
+ * Return: Pointer to newly created hash table
+ */
 
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t *array[size];
+	hash_table_t *table;
+	hash_node_t *bb;
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+	{
+		bb = malloc(sizeof(hash_node_t));
+		bb->key = NULL;
+		bb->value = NULL;
+		bb->next = NULL;
+		array[i] = bb;
+	}
+	table->size = size;
+	table->array = array;
+	return (table);
+}
