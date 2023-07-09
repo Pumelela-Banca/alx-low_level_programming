6f7c3b97fc582b72a7a7ff983d8e0f8a63630649
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-09 17:36:07 +0200

    removed printf statement

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,5 +11,23 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int size, idx;
+	hash_node_t *temp;
 
+	size = ht->size;
+	idx = key_index((unsigned char *)key, size);
+
+	if (ht->array[idx]->next == NULL)
+		return (ht->array[idx]->value);
+	else
+	{
+		temp = ht->array[idx];
+		while (temp->next != NULL)
+		{
+			if (strcmp(temp->key, (char *)key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
 }
