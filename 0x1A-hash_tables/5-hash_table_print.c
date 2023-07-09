2a3f987c084246067c7bf3c1d250d0fce4732a80
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-09 22:43:35 +0200

    prints hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,5 +10,52 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i;
+	int on = 0;
 
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i]->next != NULL &&
+				ht->array[i]->key != NULL)
+		{
+			if (on == 1)
+			{
+				printf(", ");
+				print_list(ht->array[i]);
+			}
+			else
+				print_list(ht->array[i]);
+			on = 1;
+
+		}
+		else if (ht->array[i]->key != NULL)
+		{
+			if (on == 1)
+				printf(", '%s': '%s'", ht->array[i]->key,
+					ht->array[i]->value);
+			else
+				printf("'%s': '%s'", ht->array[i]->key,
+						ht->array[i]->value);
+			on = 1;
+		}
+	}
+	printf("}\n");
+}
+
+/**
+ * print_list - prints linked list nodes
+ *
+ * @head: start of list
+ *
+ * Return: void
+ */
+
+void print_list(hash_node_t *head)
+{
+	while (head->next != NULL)
+	{
+		printf("'%s': '%s'", head->key, head->value);
+		head = head->next;
+	}
 }
