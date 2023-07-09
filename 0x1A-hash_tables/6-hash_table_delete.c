de4ba3950abc87ab9dbb5df1f4c1624e068302fd
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-09 23:02:04 +0200

    frees linked list and deletes it

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,4 +10,34 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i]->next != NULL)
+			free_list(ht->array[i]);
+		else
+			free(ht->array[i]);
+	}
+	free(ht);
+}
+
+/**
+ * free_list - fress linked list
+ *
+ * @h: first item of llist
+ *
+ * Return: void
+ */
+
+void free_list(hash_node_t *h)
+{
+	hash_node_t *temp;
+
+	while (h->next != NULL)
+	{
+		temp = h;
+		free(h);
+		h = temp->next;
+	}
 }
