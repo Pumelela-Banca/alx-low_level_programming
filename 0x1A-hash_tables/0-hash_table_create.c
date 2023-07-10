4002eccb2746fd466a10cdcc4e636c0fe3805872
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-10 22:56:01 +0200

    added condition for size == 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t *bb;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL || size == 0)
 		return (NULL);
