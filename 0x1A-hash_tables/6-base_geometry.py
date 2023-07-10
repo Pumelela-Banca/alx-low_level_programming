f6bf9476c38a8ae3748e084fa6655c577ab749b7
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-11 00:49:59 +0200

    class now has area() method that raises error

---
+++ 0x1A-hash_tables/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+a module with class to define geometry
+"""
+
+
+class BaseGeometry:
+    """
+    has method that raises exception
+    """
+
+    def area(self):
+        """
+        not implemented yet
+        """
+        raise Exception("area() is not implemented")
